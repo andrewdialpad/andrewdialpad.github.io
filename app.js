7d5ab44c5b5c3b00688db7f631ba22f7ee45d392
Andrew Paugh <andrew.paugh@dialpad.com>
2021-03-12 12:23:42 -0800

    Fix non names

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import DialpadClient from './dialpad_client.js'
 const client = new DialpadClient({env: 'sandbox'})
 client.init().then(response => {
   client.getCurrentContact().then(response => {
-    document.getElementById('contact').innerText = response.first_name
+    document.getElementById('contact').innerText = response.first_name || response.primary_phone
   })
   client.getCurrentUser().then(response => {
     document.getElementById('name').innerText = response.first_name
