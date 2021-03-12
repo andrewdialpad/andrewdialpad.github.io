a5cf5d8e8ba305d01d4a2753533425cdd61e3c38
Andrew Paugh <andrew.paugh@dialpad.com>
2021-03-12 11:52:19 -0800

    Test

--- base_request_scheduler.js
+++ base_request_scheduler.js
@@ -1,4 +1,4 @@
-import { v4 } from 'https://cdnjs.cloudflare.com/ajax/libs/uuid/8.3.2/uuid.min.js';
+import { v4 } from 'https://cdnjs.cloudflare.com/ajax/libs/uuid/8.3.2/uuidv4.min.js';
 import iframeClientUtil from './iframe_client_util.js';
 import CallbackRegistry from './callback_registry.js';
 import iframeConstants from './iframe_constants.js';
