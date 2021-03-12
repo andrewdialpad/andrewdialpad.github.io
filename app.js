bba77b3b622ad15d970a3e6ff68eaa32815ddbc7
Andrew Paugh <andrew.paugh@dialpad.com>
2021-03-12 12:14:35 -0800

    weirdness

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ import DialpadClient from './dialpad_client.js'
 
 const client = new DialpadClient({env: 'sandbox'})
 client.init().then(response => {
-  client.init().then(response => {
-    client.getCurrentUser().then((response) => {
-      console.log(response)
-    })
+  client.getCurrentUser().then((response) => {
+    console.log(response)
   })
 })
